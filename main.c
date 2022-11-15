d93ebf06bbfc2b97d13af5ee9011adedac3dd996
sue407 <sue407407@gmail.com>
2022-11-15 17:04:37 +0900

    10 (6)
    11/15 (6)

--- main.c
+++ main.c
@@ -3,20 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) {
-	char *pc;
-    int *pi;
-    double *pd;
-    
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double *)10000;
+	int i = 100;
+	int*p = &i;
+	int **q = &p;
 	
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	**q = 300;
+	printf("i = %d, *p = %d, **q= %d\n", i, *p, **q);
+	
+
 	
 	return 0;
 }
