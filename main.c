59302cb3f7ea1d0978f114f83ce6b9cbf43160fb
sue407 <sue407407@gmail.com>
2022-11-15 16:30:45 +0900

    10 (2)
    11/15 (2)

--- main.c
+++ main.c
@@ -4,21 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i = 300;
+	int *pi = &i;
+	char *pc = &i;
 	
-	int i = 10;
-	char c = 'a';
-	
-	int*iptr;
-	iptr=&i;
-	
-	char*cptr;
-	cptr=&c;
-	
-	int*iptr2;
-	iptr2=iptr;
-	
-	printf("i: %p\n%p (size:%i)\n",iptr, &i, sizeof(iptr));
-	printf("c: %p\n%p (size:%i)\n",cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
+	printf("%i, %i, %i\n", i, *pi, *pc);
 	return 0;
 }
