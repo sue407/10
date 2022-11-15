606b7ac1b3065a1c080145b887814d92c91f92dd
sue407 <sue407407@gmail.com>
2022-11-15 16:59:23 +0900

    10 (5)
    11/15 (5)

--- main.c
+++ main.c
@@ -3,24 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) {
-	int i;
-	int grade[5];
-	int accum = 0;
+	char *pc;
+    int *pi;
+    double *pd;
+    
+	pc = (char*)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
 	
-	for (i=0; i<5; i++)
-	{
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	for (i=0; i<5; i++)
-	{
-		printf("%i : %i\n", i, *(grade+i));
-		accum += *(grade+i);
-		
-	}
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	
-	printf("average : %i\n", accum/5);
 	return 0;
 }
