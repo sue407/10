c2c427193ae716de8a517a0d1bcf121fcfe45a8f
sue407 <sue407407@gmail.com>
2022-11-15 16:36:16 +0900

    10 (3)
    11/15 (3)

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int i = 300;
-	int *pi = &i;
-	char *pc = &i;
+void swap (int *x, int *y){
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
 	
-	printf("%i, %i, %i\n", i, *pi, *pc);
+}
+void main(int argc, char *argv[]) {
+	int a = 3;
+	int b = 5;
+	swap(&a,&b);
+	printf("a:%i, b:%i\n", a, b);
 	return 0;
 }
